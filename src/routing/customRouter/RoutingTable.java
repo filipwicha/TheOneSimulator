d20b6355bb2a51586a323f3b39afd9dadb34a3f9
Filip <5021567@gmail.com>
2021-02-17 19:56:13 +0100

    streamed, does not work yet

--- src/routing/customRouter/RoutingTable.java
+++ src/routing/customRouter/RoutingTable.java
@@ -1,44 +1,54 @@
 package routing.customRouter;
 
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import core.DTNHost;
 import core.SimClock;
 
 public class RoutingTable {
 
-	private ArrayList<RoutingTableEntry> routingTable;
-	public int DEFAULT_TIMEOUT = 1000000;
+	int DEFAULT_TIMEOUT = 1000000;
+	private Stream<RoutingTableEntry> routingTable;
 
 	public RoutingTable() {
-		this.routingTable = new ArrayList<>();
+		this.routingTable = Stream.empty();
 		
 		//addRoute(host, host, 0);
 	}
 
+	public static<T> Stream<T> addToBeg(Stream<T> stream, T element) {
+        return Stream.concat(Stream.of(element), stream);
+    }
+
 	public void addRoute(DTNHost destinationID, DTNHost nextHop, int hops) {
 		if (getRoute(destinationID) == null) {
-			routingTable.add(new RoutingTableEntry(destinationID, nextHop, hops));
+			this.routingTable = addToBeg(this.routingTable, new RoutingTableEntry(destinationID, nextHop, hops)); //higher probability of choosing the first entry than the last one (for optimalization purpose and starvation)
 		}
 		
 	}
 
 	public RoutingTableEntry getRoute(DTNHost destinationID) {
-		
-		
-		for (RoutingTableEntry entry : routingTable) {
-			if (entry.destinationID == destinationID) {
-				return entry;
-			}
+
+		Optional<RoutingTableEntry> result = this.routingTable.parallel().filter(entry -> entry.destinationID == destinationID ).findAny();
+
+		if(result.isPresent()){
+			return result.get();
 		}
+		
 		return null;
 	}
 
 	public void removeExpiredRoutes() {
-		for (RoutingTableEntry entry : routingTable) {
-			if (SimClock.getIntTime() - entry.sequenceNumber >= DEFAULT_TIMEOUT) {
-				routingTable.remove(entry);
-			}
-		}
+		int time = SimClock.getIntTime();
+
+		this.routingTable = this.routingTable.parallel().filter(entry -> SimClock.getIntTime() - entry.sequenceNumber >= DEFAULT_TIMEOUT );
+
+		// for (RoutingTableEntry entry : routingTable) {
+		// 	if (SimClock.getIntTime() - entry.sequenceNumber >= DEFAULT_TIMEOUT) {
+		// 		routingTable.remove(entry);
+		// 	}
+		// }
 	}
 }
